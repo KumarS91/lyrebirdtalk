0883a2cd483fecb2f86f651d944cf135a4a8abc8
Deepakkumar Subramaniam <43353758+DeepakkumarS91@users.noreply.github.com>
2018-10-24 21:08:00 -0400

    Update Reducer.js

--- src/components/Reducer/Reducer.js
+++ src/components/Reducer/Reducer.js
@@ -24,7 +24,7 @@ let voiceState = {
 export const voiceReducer = (state = voiceState , action={})=>{
     switch(action.type){
         case requestPending :
-           return {...state,isPending:true}
+            return {...state,isPending:true}
         case requestSolved :
             state.voice = []
             return {...state,
@@ -32,35 +32,30 @@ export const voiceReducer = (state = voiceState , action={})=>{
                         isPending:false,
                         hasToken : true
                     }
-
         case requestError :
             return {...state,error: action.error,isPending:false,hasToken : true}
-
         case tokenExpired :
-            return {...state,hasToken:false,isPending:false}
-        
+            return {...state,hasToken:false,isPending:false}        
         case onPostInputChange:
             return {
                 ...state,
                 postText : action.text
-            }
+                   }
         case onSearchInputChange:
             return {
                 ...state,
                 searchText : action.text
-            }
+                   }
         case clearInputs :
             return {
                 ...state,
                 searchText:'',
                 postText:''
-        }
-
+                   }
         default :
           return state
     }
 }
-
 
 const postState = {
     success : '',
@@ -79,14 +74,14 @@ export const postGenarateReducer = (state = postState,action = {}) => {
                 success : action.success,
                 loading:false,
                 hasToken:true
-            }
+                }
         case postRequestError :
             return {
             ...state,
             error : action.error,
             loading:false,
             hasToken:true
-        }
+               }
         case postTokenExpired :
             return {...state,hasToken:false,loading:false} 
         default :
